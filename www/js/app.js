ba88a05e7471764682651d3d6dd728624d84fb09
fdolzanes1 <fdolzanes1@gmail.com>
2017-08-22 10:51:05 -0400

    add plugin barcodescanner

--- www/js/app.js
+++ www/js/app.js
@@ -39,3 +39,31 @@ $('.pedir').on('click', function() {
     $('#numero-mesa').val('');
     $('.badge').remove();
 });
+
+$('.scan-qrcode').on('click', function(){
+    cordova.plugins.barcodeScanner.scan(
+       function (resultado) {
+           if (resultado.text) {
+               Materialize.toast('Mesa: ' + resultado.text, 2000);
+               $('#numero-mesa').val(resultado.text);
+           }
+       },
+       function (error) {
+           Materialize.toast('Error: ' + error, 3000, 'red-text');
+       }
+    );
+});
+
+// $('.scan-qrcode').on('click', function(){
+//     cordova.plugins.barcodeScanner.scan(
+//        function (resultado) {
+//            if (resultado.text) {
+//                Materialize.toast('Mesa: ' + resultado.text, 2000);
+//                $('#numero-mesa').val(resultado.text);
+//            }
+//        },
+//        function (error) {
+//            Materialize.toast('Error: ' + error, 3000, 'red-text');
+//        }
+//     );
+// });
